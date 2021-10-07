6b93304ead8c71683a5b4e70d170638fa5480a30
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-10-07 20:50:00 +0300

    a

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from datetime import timedelta
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -131,7 +129,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated', 
     ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ]
