378c5aba5c8a3064f82014f5519a08891e1bc0d5
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-10-08 18:28:16 +0300

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -6,5 +6,5 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
 
         if (request.method in
-        permissions.SAFE_METHODS) or obj.author == request.user:
+            permissions.SAFE_METHODS) or obj.author == request.user:
             return True
